40e8a68541221724a36cd0e1750f66539e73a333
U-LAPTOP-TCDV7JAN\ylm <yan.lamarre@gmail.com>
2018-12-25 17:24:18 -0500

    Added nsec19 platform with support for USB UART.

---
+++ src/platforms/nsec19/platform.c
@@ -0,0 +1,198 @@
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2011  Black Sphere Technologies Ltd.
+ * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This file implements the platform specific functions for the STM32
+ * implementation.
+ */
+
+#include "general.h"
+#include "cdcacm.h"
+#include "usbuart.h"
+#include "morse.h"
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/cm3/scb.h>
+#include <libopencm3/cm3/scs.h>
+#include <libopencm3/cm3/nvic.h>
+#include <libopencm3/stm32/exti.h>
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/usb/usbd.h>
+#include <libopencm3/stm32/adc.h>
+#include <libopencm3/stm32/syscfg.h>
+#include <libopencm3/stm32/flash.h>
+
+/* 
+ * 2018 - nsec18 badge
+ */
+int platform_hwversion(void)
+{
+	return 2018;
+}
+
+//Move to libopenCM3
+void rcc_clock_setup_in_hse_16mhz_out_48mhz(void)
+{
+	rcc_osc_on(RCC_HSE);
+	rcc_wait_for_osc_ready(RCC_HSE);
+	rcc_set_sysclk_source(RCC_HSE);
+
+	rcc_set_hpre(RCC_CFGR_HPRE_NODIV);
+	rcc_set_ppre(RCC_CFGR_PPRE_NODIV);
+
+	flash_set_ws(FLASH_ACR_LATENCY_024_048MHZ);
+
+	/* PLL: 16MHz * 3 = 48MHz */
+	rcc_set_pll_multiplication_factor(RCC_CFGR_PLLMUL_MUL3);
+	rcc_set_pll_source(RCC_CFGR_PLLSRC_HSE_CLK);
+	rcc_set_pllxtpre(RCC_CFGR_PLLXTPRE_HSE_CLK);
+
+	rcc_osc_on(RCC_PLL);
+	rcc_wait_for_osc_ready(RCC_PLL);
+	rcc_set_sysclk_source(RCC_PLL);
+
+	rcc_apb1_frequency = 48000000;
+	rcc_ahb_frequency = 48000000;
+}
+
+void platform_init(void)
+{
+	SCS_DEMCR |= SCS_DEMCR_VC_MON_EN;
+#ifdef ENABLE_DEBUG
+	void initialise_monitor_handles(void);
+	initialise_monitor_handles();
+#endif
+
+	rcc_periph_clock_enable(RCC_SYSCFG_COMP);
+	SYSCFG_CFGR1 |= SYSCFG_CFGR1_PA11_PA12_RMP;
+
+	rcc_clock_setup_in_hse_16mhz_out_48mhz();
+
+	/* Enable peripherals */
+	rcc_periph_clock_enable(RCC_USB);
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_GPIOB);
+	rcc_periph_clock_enable(RCC_CRC);
+	rcc_set_usbclk_source(RCC_PLL);
+
+	gpio_mode_setup(JTAG_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP,
+			TMS_DIR_PIN | TMS_PIN | TCK_PIN);
+	gpio_set_output_options(JTAG_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ,
+			TMS_DIR_PIN | TMS_PIN | TCK_PIN);
+
+	platform_timing_init();
+	cdcacm_init();
+}
+
+void platform_srst_set_val(bool assert)
+{
+	if ((platform_hwversion() == 0) ||
+	    (platform_hwversion() >= 3)) {
+		gpio_set_val(SRST_PORT, SRST_PIN, assert);
+	} else {
+		gpio_set_val(SRST_PORT, SRST_PIN, !assert);
+	}
+	if (assert) {
+		for(int i = 0; i < 10000; i++) asm("nop");
+	}
+}
+
+bool platform_srst_get_val(void)
+{
+	if (platform_hwversion() == 0) {
+		return gpio_get(SRST_SENSE_PORT, SRST_SENSE_PIN) == 0;
+	} else if (platform_hwversion() >= 3) {
+		return gpio_get(SRST_SENSE_PORT, SRST_SENSE_PIN) != 0;
+	} else {
+		return gpio_get(SRST_PORT, SRST_PIN) == 0;
+	}
+}
+
+bool platform_target_get_power(void)
+{
+	return 0;
+}
+
+void platform_target_set_power(bool power)
+{
+    (void) power;
+}
+
+const char *platform_target_voltage(void)
+{
+	return "Unsupported";
+}
+
+void platform_request_boot(void)
+{
+}
+
+#ifdef ENABLE_DEBUG
+enum {
+	RDI_SYS_OPEN = 0x01,
+	RDI_SYS_WRITE = 0x05,
+	RDI_SYS_ISTTY = 0x09,
+};
+
+int rdi_write(int fn, const char *buf, size_t len)
+{
+	(void)fn;
+	if (debug_bmp)
+		return len - usbuart_debug_write(buf, len);
+
+	return 0;
+}
+
+struct ex_frame {
+	union {
+		int syscall;
+		int retval;
+	};
+	const int *params;
+	uint32_t r2, r3, r12, lr, pc;
+};
+
+void debug_monitor_handler_c(struct ex_frame *sp)
+{
+	/* Return to after breakpoint instruction */
+	sp->pc += 2;
+
+	switch (sp->syscall) {
+	case RDI_SYS_OPEN:
+		sp->retval = 1;
+		break;
+	case RDI_SYS_WRITE:
+		sp->retval = rdi_write(sp->params[0], (void*)sp->params[1], sp->params[2]);
+		break;
+	case RDI_SYS_ISTTY:
+		sp->retval = 1;
+		break;
+	default:
+		sp->retval = -1;
+	}
+
+}
+
+asm(".globl debug_monitor_handler\n"
+    ".thumb_func\n"
+    "debug_monitor_handler: \n"
+    "    mov r0, sp\n"
+    "    b debug_monitor_handler_c\n");
+
+#endif
---
+++ src/platforms/nsec19/platform.h
@@ -0,0 +1,171 @@
+/*
+ * This file is part of the Black Magic Debug project.
+ *
+ * Copyright (C) 2011  Black Sphere Technologies Ltd.
+ * Written by Gareth McMullin <gareth@blacksphere.co.nz>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* This file implements the platform specific functions for the STM32
+ * implementation.
+ */
+#ifndef __PLATFORM_H
+#define __PLATFORM_H
+
+#include "gpio.h"
+#include "timing.h"
+#include "timing_stm32.h"
+
+// #define PLATFORM_HAS_TRACESWO
+// #define PLATFORM_HAS_POWER_SWITCH
+#ifdef ENABLE_DEBUG
+#define PLATFORM_HAS_DEBUG
+#define USBUART_DEBUG
+#endif
+#define BOARD_IDENT             "NorthSec 2018 Probe"
+#define BOARD_IDENT_DFU	        "NorthSec 2018 Probe (Upgrade)"
+#define BOARD_IDENT_UPD	        "NorthSec 2018 Probe (DFU Upgrade)"
+#define DFU_IDENT               "NorthSec 2018 Firmware Upgrade"
+#define DFU_IFACE_STRING        "@Internal Flash   /0x08000000/8*001Ka,120*001Kg"
+#define UPD_IFACE_STRING        "@Internal Flash   /0x08000000/8*001Kg"
+
+/* Important pin mappings for NorthSec badge implementation:
+ * uC is a STM32F070F6P6.
+ *
+ * nFR52 (internal) SWDIO/TMS: PA5
+ * nFR52 (internal) SWCLK/TCK: PA6
+ * nFR52 (internal) nRST: PA7
+ *
+ * External SWDIO/TMS: PB12
+ * External SWCLK/TCK: PB15
+ * External nRST: PB2
+ *
+ * USB cable -: PA11
+ * USB cable +: PA12
+ */
+
+/* Hardware definitions... */
+#define JTAG_PORT 	GPIOA
+#define TDI_PORT	JTAG_PORT
+#define TMS_DIR_PORT	JTAG_PORT
+#define TMS_PORT	JTAG_PORT
+#define TCK_PORT	JTAG_PORT
+#define TDO_PORT	JTAG_PORT
+// #define TDI_PIN		GPIO3
+#ifdef NSEC_BADGE_USE_EXTERNAL_PINS
+#define TMS_DIR_PORT	GPIOB
+#define TMS_DIR_PIN	GPIO1
+#define TMS_PIN		GPIO4
+#define TCK_PIN		GPIO5
+#else
+#define TMS_DIR_PORT	JTAG_PORT
+#define TMS_DIR_PIN	GPIO4
+#define TMS_PIN		GPIO1
+#define TCK_PIN		GPIO2
+#endif
+
+// #define TDO_PIN		GPIO6
+
+#define SWDIO_DIR_PORT	JTAG_PORT
+#define SWDIO_PORT 	JTAG_PORT
+#define SWCLK_PORT 	JTAG_PORT
+#define SWDIO_DIR_PIN	TMS_DIR_PIN
+#define SWDIO_PIN	TMS_PIN
+#define SWCLK_PIN	TCK_PIN
+
+#define SRST_PORT	GPIOA
+#define SRST_PIN	GPIO2
+#define SRST_SENSE_PORT	GPIOA
+#define SRST_SENSE_PIN	GPIO7
+
+#define TMS_SET_MODE() do { \
+	gpio_set(TMS_DIR_PORT, TMS_DIR_PIN); \
+	gpio_mode_setup(TMS_DIR_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, TMS_PIN); \
+	gpio_set_output_options(TMS_DIR_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, \
+			TMS_PIN); \
+} while(0)
+
+#define SWDIO_MODE_FLOAT() do { \
+	gpio_mode_setup(SWDIO_PORT, GPIO_MODE_INPUT, GPIO_PUPD_NONE, SWDIO_PIN); \
+	gpio_clear(SWDIO_DIR_PORT, SWDIO_DIR_PIN); \
+} while(0)
+
+#define SWDIO_MODE_DRIVE() do { \
+	gpio_set(SWDIO_DIR_PORT, SWDIO_DIR_PIN); \
+	gpio_mode_setup(SWDIO_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_PULLUP, SWDIO_PIN); \
+	gpio_set_output_options(SWDIO_PORT, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, \
+			SWDIO_PIN); \
+} while(0)
+
+#define USB_DRIVER st_usbfs_v2_usb_driver
+#define USB_IRQ    NVIC_USB_IRQ
+#define USB_ISR    usb_isr
+/* Interrupt priorities.  Low numbers are high priority.
+ * For now USART1 preempts USB which may spin while buffer is drained.
+ * TIM3 is used for traceswo capture and must be highest priority.
+ */
+#define IRQ_PRI_USB             (2 << 6)
+#define IRQ_PRI_USBUSART	(1 << 4)
+#define IRQ_PRI_USBUSART_TIM	(3 << 4)
+#define IRQ_PRI_TRACE           (0 << 5)
+
+#define USBUSART USART2
+#define USBUSART_CR1 USART2_CR1
+#define USBUSART_IRQ NVIC_USART2_IRQ
+#define USBUSART_CLK RCC_USART2
+#define USBUSART_TX_PORT GPIOA
+#define USBUSART_TX_PIN  GPIO2
+#define USBUSART_RX_PORT GPIOA
+#define USBUSART_RX_PIN  GPIO9
+#define USBUSART_ISR usart2_isr
+#define USBUSART_TIM TIM7
+#define USBUSART_TIM_CLK_EN() rcc_periph_clock_enable(RCC_TIM7)
+#define USBUSART_TIM_IRQ NVIC_TIM7_IRQ
+#define USBUSART_TIM_ISR tim7_isr
+
+#define UART_PIN_SETUP() do { \
+	gpio_mode_setup(USBUSART_TX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, \
+	                USBUSART_TX_PIN); \
+	gpio_mode_setup(USBUSART_RX_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, \
+	                USBUSART_RX_PIN); \
+	gpio_set_af(USBUSART_TX_PORT, GPIO_AF1, USBUSART_TX_PIN); \
+	gpio_set_af(USBUSART_RX_PORT, GPIO_AF1, USBUSART_RX_PIN); \
+    } while(0)
+
+#define TRACE_TIM TIM3
+#define TRACE_TIM_CLK_EN() rcc_periph_clock_enable(RCC_TIM3)
+#define TRACE_IRQ   NVIC_TIM3_IRQ
+#define TRACE_ISR   tim3_isr
+
+#ifdef ENABLE_DEBUG
+extern bool debug_bmp;
+int usbuart_debug_write(const char *buf, size_t len);
+
+#define DEBUG printf
+#else
+#define DEBUG(...)
+#endif
+
+#define SET_RUN_STATE(state)	{running_status = (state);}
+#define SET_IDLE_STATE(state)
+#define SET_ERROR_STATE(state)
+
+/* Use newlib provided integer only stdio functions */
+#define sscanf siscanf
+#define sprintf siprintf
+#define snprintf sniprintf
+#define vasprintf vasiprintf
+
+#endif
